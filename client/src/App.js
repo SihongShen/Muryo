b504eb63e6932420dfa216eeec93690ebe20d27c
SihongShen <ss18264@nyu.edu>
2025-12-10 20:10:19 -0500

    update UI and logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom';
 import './App.css';
 import Background from './components/background.jsx';
 import Homepage from './pages/homepage/homepage.jsx';
@@ -9,12 +9,40 @@ import UserPage from './pages/user/user.jsx';
 import LoginPage from './pages/login/login.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 
+const Layout = () => {
+  const logoColor = '#0a3a40'; 
+
+  return (
+    <>
+      <Link 
+        to="/" 
+        style={{
+            position: 'fixed',
+            top: '24px',
+            left: '80px',
+            zIndex: 1000,
+            textDecoration: 'none',
+            color: logoColor,
+            fontSize: '2.2rem',
+            fontWeight: '900',
+            letterSpacing: '1px',
+            fontFamily: '"Helvetica Neue", sans-serif',
+            textShadow: '0 5px 10px rgba(255, 255, 255, 0.67)'
+        }}
+      >
+        無料MEMO
+      </Link>
+      <Background />
+    </>
+  );
+};
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Background />}>
+          <Route path="/" element={<Layout />}>
             
             <Route index element={<Homepage />} />
             
